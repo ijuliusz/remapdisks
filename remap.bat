7e8bf6ee25440da9094d2a94837269f79e04b2bb
ijuliusz <33216868+ijuliusz@users.noreply.github.com>
2021-06-03 22:35:11 +0200

    Create remap.bat
    Batch code to remapping net drives.
    Set new IP address for Your new server
    Old drive letters and connected folders to them will stay.
    Only old server IP will be replaced with the new one

---
+++ remap.bat
@@ -0,0 +1,17 @@
+@echo off
+SETLOCAL ENABLEDELAYEDEXPANSION
+
+set "newIP=192.168.3.16"
+
+FOR /f "tokens=1-4" %%a IN ('net use') do (call :replaceIP %%b %%c)
+goto :end
+
+:replaceIP
+set "driveLetter=%~1"
+set "serverIP=%~2"
+net use %driveLetter% /delete /y
+for /f delims^=^\^ tokens^=1 %%p in ("%serverIP%") do (set oldIP=%%p)
+call set serverIP=%%serverIP:!oldIP!=!newIP!%%
+net use %driveLetter% %serverIP% /p:yes
+
+:end
